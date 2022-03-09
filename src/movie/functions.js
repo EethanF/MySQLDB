5896cae0780a03f8303e18d3318a43304dd5b47d
Ethan Frediani <91057765+EethanF@users.noreply.github.com>
2022-03-09 11:20:07 +0000

    Update functions.js

--- src/movie/functions.js
+++ src/movie/functions.js
@@ -3,45 +3,42 @@ const Movie = require("./table");
 
 exports.addMovie = async (movieObj) => {
     try {
-        await Movie.create(movieObj); //make argument in app.js
+        await Movie.create(movieObj);
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 exports.listMovies = async (filterObj) => {
     console.log("filterObj: ", filterObj);
     try {
-        if (filterObj.undefined === undefined) {
+        if (filterObj.title || filterObj.actor) {
+            return await Movie.findOne({ where: filterObj });
+        } else {
             return await Movie.findAll();
-        } else {
-            return await Movie.findOne({where: filterObj});
         }
     } catch (error) {
         console.log(error);
     }
-}
+};
 
-exports.deleteMovie = async (deleteObj) => {
+exports.updateMovie = async (filterObj) => {
     try {
-        if (deleteObj.title || deleteObj.actor) {
-            return await Movie.destroy({where: deleteObj});
+        if (filterObj.newTitle ) {
+        return await Movie.update({ title: filterObj.newTitle },{ where: { title: filterObj.oldTitle } });
+        } else if (filterObj.newActor) {
+        return await Movie.update( { actor: filterObj.newActor },{ where: { actor: filterObj.oldActor } });
         } else {
-            console.log("Input Incorrect");
-        }
+            console.log("incorrect input");
+        };
     } catch (error) {
         console.log(error);
     }
-}
+};
 
-exports.updateMovie = async (param, filterObj, update) => {
+exports.deleteMovie = async (title) => {
     try {
-        if (param === "title"){
-        await Movie.update({title: update}, {where: {title: filterObj}});
-        } else if (param === "actor") {
-            const test =  await Movie.update({actor: update}, {where: {actor: filterObj}});
-            console.log(test)
-        }
+        return await Movie.destroy({ where: title });
     } catch (error) {
         console.log(error);
     }
