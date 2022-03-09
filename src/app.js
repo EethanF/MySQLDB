223fd73928d57af319db7a6cd67ffb418479ea5c
EethanF <ethan_frediani@yahoo.co.uk>
2022-03-09 10:26:18 +0000

    finished mysql

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const yargs = require("yargs")
+
+const { sequelize } = require("./db/connection")
+const { addMovie, listMovies, updateMovie, deleteMovie } = require("./movie/movieFunctions")
+
+const app = async (yargsObj) => {
+    try {
+        await sequelize.sync()
+        if(yargsObj.add) {
+            await addMovie({title: yargsObj.title, actor: yargsObj.actor});
+            console.log(JSON.stringify(await listMovies(), null, 2));
+        } else if (yargsObj.list) {
+            console.log(JSON.stringify(await listMovies({[yargsObj.key]: yargsObj.value}), null, 2))
+        } else if (yargsObj.update) {
+            console.log(await updateMovie(yargsObj.param, yargsObj.filterObj, yargsObj.update))
+        } else if (yargsObj.delete) {
+            await deleteMovie({[yargsObj.key]: yargsObj.value})
+            console.log(JSON.stringify(await listMovies(), null, 2));
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+app(yargs.argv)
---
+++ src/movie/functions.js
@@ -0,0 +1,48 @@
+const Movie = require("./table");
+
+
+exports.addMovie = async (movieObj) => {
+    try {
+        await Movie.create(movieObj); //make argument in app.js
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.listMovies = async (filterObj) => {
+    console.log("filterObj: ", filterObj);
+    try {
+        if (filterObj.undefined === undefined) {
+            return await Movie.findAll();
+        } else {
+            return await Movie.findOne({where: filterObj});
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.deleteMovie = async (deleteObj) => {
+    try {
+        if (deleteObj.title || deleteObj.actor) {
+            return await Movie.destroy({where: deleteObj});
+        } else {
+            console.log("Input Incorrect");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.updateMovie = async (param, filterObj, update) =>{
+    try {
+        if (param === "title"){
+        await Movie.update({title: update}, {where: {title: filterObj}});
+        } else if (param === "actor") {
+            const test =  await Movie.update({actor: update}, {where: {actor: filterObj}});
+            console.log(test)
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
